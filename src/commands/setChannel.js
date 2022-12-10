ac2213c10c5ae9d8e830669be02f03bf47bf4d8a
Huguitis <65133800+Huguitis@users.noreply.github.com>
2022-12-10 16:04:52 +0100

    Fix some things

--- src/commands/setChannel.js
+++ src/commands/setChannel.js
@@ -10,6 +10,8 @@ $description[1;$getVar[SuccessEmoji] **The global chat channel is now configured
 $slowmode[5s;$findServerChannel[$message]]
 $setServerVar[GlobalChatChannelID;$findServerChannel[$message]]
 
+$onlyIf[$getServerVar[GlobalChatChannelID]!=$findServerChannel[$message];{newEmbed:{color:RED}{description:$getVar[ErrorEmoji] **__That channel is already configured for the global chat**}}]
+
 $onlyIf[$hasAnyPerm[$guildID;$clientID;managechannel;manageserver;admin]!=false;{newEmbed:{color:RED}{description:$getVar[ErrorEmoji] **__I need at least one of the following permissions to be able to execute this command:__ "Manage Channels", "Manage Server" or "Admin"**}}]
 
 $onlyIf[$hasAnyPerm[$guildID;$authorID;managechannel;manageserver;admin]!=false;{newEmbed:{color:RED}{description:$getVar[ErrorEmoji] **__You need at least one of the following permissions to be able to execute this command:__ "Manage Channels", "Manage Server" or "Admin"**}}]
