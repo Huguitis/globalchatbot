b5838c56a622947ff7911f050683073e72d6137c
Huguitis <65133800+Huguitis@users.noreply.github.com>
2022-12-10 15:55:04 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+    BotToken: "",
+    ImgBBApiKey: "",
+    SupportServerInvite: "https://discord.gg/SRNA8KWHVh",
+    BlockedContent: "fuck, shit, bitch",
+
+    SuccessEmoji: "",
+    ErrorEmoji: "",
+    LoadingEmoji: "",
+    NotifyEmoji: "",
+    RoleUserEmoji: "",
+    RoleModeratorEmoji: "",
+    RoleOwnerEmoji: ""
+  }
